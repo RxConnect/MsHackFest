a6e5cfdef517d5114ee7b048457ddc6df3f3e817
PepLluis <PepLluis@LAPTOP-5J0HL6UM>
2018-01-23 15:25:47 +0100

    Initial Code

---
+++ OpenDoors_Xforms/OpenDoors_Xforms/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace OpenDoors_Xforms
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new OpenDoors_Xforms.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
