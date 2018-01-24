014b22979382f9deca1487ea438ae9bbb57a3810
PepLluis <PepLluis@LAPTOP-5J0HL6UM>
2018-01-24 13:04:35 +0100

    API / Data Models

---
+++ RxConnectSite/Data/RxDbInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RxConnectSite.Models;
+
+namespace RxConnectSite.Data
+{
+    public class RxDbInitializer
+    {
+        public static void Initialize(RxConnectSiteContext dbContext)
+        {
+            dbContext.Database.EnsureCreated();
+
+            if (dbContext.Fobs.Any())
+            {
+                //db already initialized
+                return;
+            }
+
+            dbContext.Fobs.Add(new Fobs() { Command = CommandType.Activation, DeviceNumber = "12345" });
+            dbContext.SaveChanges();
+        }
+    }
+}
